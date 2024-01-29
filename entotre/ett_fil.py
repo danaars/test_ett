24d57c40f493a2d468bab4597660f7d322c09bcd
danaars <69962703+danaars@users.noreply.github.com>
2024-01-29 13:50:10 +0000

    import in only init

--- entotre/ett_fil.py
+++ entotre/ett_fil.py
@@ -1,13 +1,15 @@
 import sys
 
-sys.path.append(".")
-print("Path added")
+#sys.path.append(".")
+#print("Path added")
 import random
-from . import annen_fil
+#from . import annen_fil
+from annen_fil import niceprint
 
 def ett():
     s = random.choice([1, 2, 3, "en", "to", "tre"])
-    annen_fil.niceprint(s)
+    #annen_fil.niceprint(s)
+    niceprint(s)
 
 if __name__ == "__main__":
     ett()
