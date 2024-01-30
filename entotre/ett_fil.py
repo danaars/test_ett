dd8aff8b91d3c28a9993e936c4e21fa8eceb203c
danaars <69962703+danaars@users.noreply.github.com>
2024-01-30 09:38:46 +0000

    full path import, similar to what works locally

--- entotre/ett_fil.py
+++ entotre/ett_fil.py
@@ -4,14 +4,16 @@
 #print("Path added")
 import random
 #from . import annen_fil
-from submodule.submodfile import submodfunc
-print("submod import succ")
-from annen_fil import niceprint
+from entotre.submodule.submodfile import submodfunc
+#print("submod import succ")
+from entotre.annen_fil import niceprint
 
 def ett():
     s = random.choice([1, 2, 3, "en", "to", "tre"])
     #annen_fil.niceprint(s)
     niceprint(s)
+    if isinstance(s, int):
+        print(f"SubModFunc of {s}: {submodfunc(s)}")
 
 if __name__ == "__main__":
     ett()
--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name = "GiTall",
-    version = "3.6",
+    version = "3.7",
     packages = find_packages(),
-    install_requires = [
-        'pip @ git+https://github.com/danaars/test_ett.git#subdirectory=submodule'
-    ]
 )
