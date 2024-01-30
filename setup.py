ba9eb03481b5bcac7b2f86c176718a6000f2e35a
danaars <69962703+danaars@users.noreply.github.com>
2024-01-30 08:26:57 +0000

    added install_require

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name = "GiTall",
-    version = "3",
+    version = "3.1",
     packages = find_packages(),
+    install_requires = [
+        'https://github.com/danaars/test_ett'
+    ]
 )
